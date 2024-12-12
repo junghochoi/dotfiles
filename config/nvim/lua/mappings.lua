39824d05b2db713713e4553bc514d45869dfc950
c3-daniel-choi <daniel.choi@c3.ai>
2024-12-12 14:26:00 -0500

    update

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -10,14 +10,18 @@ map("n", "J", "5j")
 map("n", "L", "5l")
 map("n", "H", "5h")
 
+map("v", "K", "5k")
+map("v", "J", "5j")
+map("v", "L", "5l")
+map("v", "H", "5h")
 -- tmux navigator
-map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>" )
-map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>" )
-map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>" )
-map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>" )
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
 
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>") -- change live_grep keybinding
-nomap("n","<leader>fw") -- remove original keybinding
+nomap("n", "<leader>fw") -- remove original keybinding
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- nomap("n", "<leader>v") -- disable vertical terminal
@@ -28,11 +32,8 @@ nomap("n", "<M-v>") -- disable vertical terminal
 nomap("n", "<M-h>") -- disable horizontal terminal
 nomap("n", "<M-i>") -- disable floating terminal
 
-
 map("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-
 
 map("n", "<C-t>", function()
   require("nvchad.themes").open()
 end, {})
-
