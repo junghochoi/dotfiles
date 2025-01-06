b5791eb45e045357dd4304335c32db800f162405
c3-daniel-choi <daniel.choi@c3.ai>
2025-01-06 15:01:03 -0500

    add yabai and skhd to install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ sudo rm -rf ~/.config/starship.toml
 
 sudo rm -rf ~/.config/nvim
 sudo rm -rf ~/.config/tmux
-
+sudo rm -rf ~/.config/skhd
+sudo rm -rf ~/.config/yabai
 
 echo "Creating symlinks"
 ln -sf ~/dotfiles/config/.gitconfig  		~/.gitconfig
@@ -14,9 +15,10 @@ ln -sf ~/dotfiles/config/starship.toml  ~/.config/starship.toml
 
 ln -sf ~/dotfiles/config/nvim	  ~/.config/nvim
 ln -sf ~/dotfiles/config/tmux 	~/.config/tmux
+ln -sf ~/dotfiles/config/skhd   ~/.config/skhd
+ln -sf ~/dotfiles/config/yabai  ~/.config/yabai
 
 echo "Creating TMUX_CONF env variable"
 export TMUX_CONF=~/.config/tmux/tmux.conf
-
 
 echo "Done"
