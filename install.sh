5bf0c4c0679272000820ff81340a2a68cdcb4c56
c3-daniel-choi <daniel.choi@c3.ai>
2025-01-06 14:49:37 -0500

    WIP

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "Removing Files"
 sudo rm -rf ~/.zshrc
 sudo rm -rf ~/.gitconfig
 sudo rm -rf ~/.config/starship.toml
@@ -6,9 +7,16 @@ sudo rm -rf ~/.config/nvim
 sudo rm -rf ~/.config/tmux
 
 
+echo "Creating symlinks"
 ln -sf ~/dotfiles/config/.gitconfig  		~/.gitconfig
 ln -sf ~/dotfiles/config/.zshrc 		    ~/.zshrc
 ln -sf ~/dotfiles/config/starship.toml  ~/.config/starship.toml
 
 ln -sf ~/dotfiles/config/nvim	  ~/.config/nvim
 ln -sf ~/dotfiles/config/tmux 	~/.config/tmux
+
+echo "Creating TMUX_CONF env variable"
+export TMUX_CONF=~/.config/tmux/tmux.conf
+
+
+echo "Done"
