ef9ae2ef78cc16b80d679ad9bc10b7391aa509f4
c3-daniel-choi <daniel.choi@c3.ai>
2025-04-25 10:34:25 -0400

    Enhance the install script

--- install.sh
+++ install.sh
@@ -1,25 +1,60 @@
-echo "Removing Files"
-sudo rm -rf ~/.zshrc
-sudo rm -rf ~/.gitconfig
-sudo rm -rf ~/.config/starship.toml
+# HELPER Functions
 
-sudo rm -rf ~/.config/nvim
-sudo rm -rf ~/.config/tmux
-sudo rm -rf ~/.config/skhd
-sudo rm -rf ~/.config/yabai
 
-echo "Creating symlinks"
-ln -s ~/github/dotfiles/config/.gitconfig  		~/.gitconfig
-ln -s ~/github/dotfiles/config/.zshrc 		    ~/.zshrc
-ln -s ~/github/dotfiles/config/starship.toml  ~/.config/starship.toml
+echo_color() {
+  local color=$1
+  local message=$2
 
-ln -s ~/github/dotfiles/config/nvim	  ~/.config/nvim
-ln -s ~/github/dotfiles/config/tmux 	  ~/.config/tmux
+  case "$color" in
+    red)    code="\033[0;31m" ;;
+    green)  code="\033[0;32m" ;;
+    yellow) code="\033[0;33m" ;;
+    blue)   code="\033[0;34m" ;;
+    magenta) code="\033[0;35m" ;;
+    cyan)   code="\033[0;36m" ;;
+    bold)   code="\033[1m" ;;
+    *)      code="\033[0m" ;; # default (no color)
+  esac
+
+  printf "${code}%s\033[0m\n" "$message"
+}
+
+link_config() {
+  local source=$1
+  local target=$2
+  local name=$3
+
+  echo_color "blue" "Setting up config - $name"
+
+  if [ -L "$target" ] || [ -d "$target" ]; then
+    echo "Removing $target"
+    rm -rf "$target"
+  fi
+
+  echo "Linking $target → $source"
+  ln -s "$source" "$target"
+}
+
+# MAIN
+if [[ "$(pwd)" != "$HOME/dotfiles"* ]]; then
+    echo "Error: Script must be run from within ~/dotfiles"
+      exit 1
+fi
+
+echo "Linking dotfiles..."
+
+
+link_config "$HOME/dotfiles/config/.gitconfig"    "$HOME/.gitconfig"              "git"
+link_config "$HOME/dotfiles/config/.zshrc"        "$HOME/.zshrc"                  "zsh"
+link_config "$HOME/dotfiles/config/starship.toml" "$HOME/.config/starship.toml"   "starship"
+link_config "$HOME/dotfiles/config/nvim"          "$HOME/.config/nvim"            "nvim"
+link_config "$HOME/dotfiles/config/tmux"          "$HOME/.config/tmux"            "tmux"
+
 
 
 # Archived Configurations. To enable, move config files from ./archive_config to ./config/
-# ln -s ~/github/dotfiles/config/skhd           ~/.config/skhd
-# ln -s ~/github/dotfiles/config/yabai   ~/.config/yabai
+# ln -s ~/dotfiles/config/skhd           ~/.config/skhd
+# ln -s ~/dotfiles/config/yabai   ~/.config/yabai
 
 echo "Creating TMUX_CONF env variable"
 export TMUX_CONF=~/.config/tmux/tmux.conf
