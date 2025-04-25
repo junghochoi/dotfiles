d12c388a8e22a06d06011e619df1276fa6dd414c
c3-daniel-choi <daniel.choi@c3.ai>
2025-04-25 09:40:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,11 @@ ln -s ~/github/dotfiles/config/starship.toml  ~/.config/starship.toml
 
 ln -s ~/github/dotfiles/config/nvim	  ~/.config/nvim
 ln -s ~/github/dotfiles/config/tmux 	  ~/.config/tmux
-ln -s ~/github/dotfiles/config/skhd           ~/.config/skhd
-ln -s ~/github/dotfiles/config/yabai   ~/.config/yabai
+
+
+# Archived Configurations. To enable, move config files from ./archive_config to ./config/
+# ln -s ~/github/dotfiles/config/skhd           ~/.config/skhd
+# ln -s ~/github/dotfiles/config/yabai   ~/.config/yabai
 
 echo "Creating TMUX_CONF env variable"
 export TMUX_CONF=~/.config/tmux/tmux.conf
