dd7096a587f7fbb81097ef2627fbe61d4e8b63a3
c3-daniel-choi <daniel.choi@c3.ai>
2025-09-15 18:32:28 +0000

    update

--- config/nvim/lua/plugins/google/telescope-codesearch.lua
+++ config/nvim/lua/plugins/google/telescope-codesearch.lua
@@ -5,6 +5,18 @@ return {
 		-- lazy.nvim relies on a declarative api (LazySpec) to configure your
 		-- plugins. See https://github.com/folke/lazy.nvim#-plugin-spec for
 		-- more information about the available options.
+		keys = {
+			{
+				"<leader>cs",
+				"<cmd>Telescope codesearch find_files<cr>",
+				desc = "Find codesearch files",
+			},
+			{
+				"<leader>cg",
+				"<cmd>Telescope codesearch find_query<cr>",
+				desc = "Find codesearch query",
+			},
+		},
 		config = function()
 			-- This asks telescope to load the codesearch extension and makes
 			-- the 'codesearch' picker available through the `Telescope` command.
