2aa6dd9080eed749a52c788fe4bdc955de50c219
c3-daniel-choi <daniel.choi@c3.ai>
2025-09-15 18:46:52 +0000

    telescope is working properly

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -11,8 +11,8 @@ map("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
 map("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
 map("n", "<leader>fb", telescope.buffers, { desc = "Telescope buffers" })
 map("n", "<leader>fh", telescope.help_tags, { desc = "Telescope help tags" })
-map("n", "<leader>cs",  "<cmd>Telescope codesearch find_files<cr>",  { desc = "Telescope Codesearch" })
-map("n", "<leader>cg",  "<cmd>Telescope codesearch find_query<cr>",  { desc = "Telescope Codesearch" })
+map("n", "<leader>ss",  "<cmd>Telescope codesearch find_files<cr>",  { desc = "Telescope Codesearch" })
+map("n", "<leader>sd",  "<cmd>Telescope codesearch find_query<cr>",  { desc = "Telescope Codesearch" })
 
 
 
--- config/nvim/lua/plugins/google/telescope-codesearch.lua
+++ config/nvim/lua/plugins/google/telescope-codesearch.lua
@@ -1,30 +1,3 @@
--- return {
--- 	-- This is the correct, standard way to specify a GitHub plugin.
--- 	-- lazy.nvim automatically infers the URL (https://github.com/vintharas/...)
--- 	"vintharas/telescope-codesearch.nvim",
---
--- 	-- The 'url' and 'sso://' line should be completely removed.
--- 	-- url = "sso://user/vintharas/telescope-codesearch.nvim",
---   opts = {
---
---
--- 	keys = {
--- 		{
--- 			"<leader>fc",
--- 			"<cmd>Telescope codesearch find_files<cr>",
--- 			desc = "Find codesearch files",
--- 		},
--- 		{
--- 			"<leader>fcc",
--- 			"<cmd>Telescope codesearch find_query<cr>",
--- 			desc = "Find codesearch query",
--- 		},
--- 	},
--- 	config = function()
--- 		require("telescope").load_extension("codesearch")
--- 	end,
--- }
-
 return {
 	{
 		-- "vintharas/telescope-codesearch.nvim",
@@ -32,18 +5,6 @@ return {
 		-- lazy.nvim relies on a declarative api (LazySpec) to configure your
 		-- plugins. See https://github.com/folke/lazy.nvim#-plugin-spec for
 		-- more information about the available options.
-		keys = {
-			{
-				"<leader>cs",
-				"<cmd>Telescope codesearch find_files<cr>",
-				desc = "Find codesearch files",
-			},
-			{
-				"<leader>cg",
-				"<cmd>Telescope codesearch find_query<cr>",
-				desc = "Find codesearch query",
-			},
-		},
 		config = function()
 			-- This asks telescope to load the codesearch extension and makes
 			-- the 'codesearch' picker available through the `Telescope` command.
