0f9eaeb2e8df8c80a1052a60fb35865dcda778da
c3-daniel-choi <daniel.choi@c3.ai>
2025-09-15 17:09:11 +0000

    update from laptop

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,5 @@
 require("config.lazy")  -- Installs Lazy
 require("config.lspconfig") -- Configures LSP
--- require("config.cmp")  -- Configure Completion Sources
 
 require("core.mappings") -- Configure key mappings
 require("core.options") -- Configure Vim Options
